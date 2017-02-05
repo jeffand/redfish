e9e527e4f78737cc3689cd851a4dca28df653749
jeffand <jeffand@gmail.com>
2017-02-05 00:51:37 -0800

    Updated with serial number verification

--- mdbparse.rb
+++ mdbparse.rb
@@ -1,14 +1,33 @@
 #!/usr/bin/ruby
+require "open-uri"
+require "json"
+require "colorize"
+
+system "clear"
 
 iloHostName = ARGV[0]
 
 #file = File.open(iloHostName, 'r')
-mdbTable = Hash.new
+mdbTable,systemInfo = Hash.new
 
 File.open(iloHostName).each_line do |line|
 	param,value = line.chomp.split(":", 2 )
 	#mdbtable = Hash[*value.flatten]
 	mdbTable[param] = value.strip	
-	puts "Parameter: #{param} = #{mdbTable[param]} "
 end
 
+puts "MDB Host values read\n".yellow
+
+iloResponse = open('http://localhost:32768/redfish/v1/Systems/437XR1138R2/').read
+
+systemInfo = JSON.parse(iloResponse)
+
+puts "Serial Number from system = #{systemInfo["SerialNumber"]} \n".yellow
+
+puts "Serial Number from file = #{mdbTable["serialNumber"]} \n".yellow
+
+if mdbTable["serialNumber"] == systemInfo["SerialNumber"]
+	puts "Serial Numbers Match".green
+else
+	puts "Serial Numbers Don't Match".red
+end
