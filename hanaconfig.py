c4c65f04b95695193c599cb5dc89c8e576897e6e
jeffand <jeffand@gmail.com>
2017-03-17 11:04:23 -0700

    small change

--- hanaconfig.py
+++ hanaconfig.py
@@ -8,7 +8,7 @@ ENV = Environment(loader=FileSystemLoader('./'))
 #    dict =  yaml.load(_)
 dict = {}
 
-#dict = { 'fqdn_hostname' : 'hana106.lab1.ariba.com', 'datacenter': 'lab1'}
+#dict = { 'fqdn_hostname' : 'hana106.lab1.riba.com', 'datacenter': 'lab1'}
 #currentDatacenterName = socket.getfqdn().split('.')[1]
 dict['fqdn_hostname'] = socket.getfqdn()
 dict['datacenter'] = socket.getfqdn().split('.')[1]
