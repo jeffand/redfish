e194ed4e027458904e1700eed9ef534d47c7b762
jeffand <jeffand@gmail.com>
2017-03-17 11:23:04 -0700

    Clean up

--- hanaconfig.py
+++ hanaconfig.py
@@ -4,17 +4,14 @@ import socket
 
 ENV = Environment(loader=FileSystemLoader('./'))
 
-#with open("config.yaml") as _:
-#    dict =  yaml.load(_)
+# Init the dictionary
 dict = {}
 
 #dict = { 'fqdn_hostname' : 'hana106.lab1.riba.com', 'datacenter': 'lab1'}
-#currentDatacenterName = socket.getfqdn().split('.')[1]
+
 dict['fqdn_hostname'] = socket.getfqdn()
 dict['datacenter'] = socket.getfqdn().split('.')[1]
 
-# Print dictionary generated from yaml
-print dict
 
 # Render template and print generated config to console
 template = ENV.get_template("hana.text")
